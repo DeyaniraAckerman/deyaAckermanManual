e38fcd9e76782dd65b286c657dc867f294d402ee
DeyaniraAckerman <deyaniaAckerman@gmail.com>
2021-01-16 23:46:33 -0600

    initial commit

---
+++ UTTT.Ejemplo.Persona.Control/Ctrl/Conexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UTTT.Ejemplo.Persona.Control.Ctrl
+{
+    public class Conexion
+    {
+        #region Variables
+     
+        #endregion
+
+        #region Constructores
+        public Conexion()
+        {
+        }       
+        #endregion
+
+
+        public SqlConnection sqlConnection()
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection("data source=DESKTOP-C9NJTC8\\SQLSERVER;initial catalog=Manual;user id=sa;Password=1234");
+                return conexion;
+            }
+            catch (Exception _e)
+            { 
+            
+            }
+            return null;
+        }
+    }
+}
